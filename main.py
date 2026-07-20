aaac4f2a215e5175d71115e7254247fe22c59183
Rohit Kushwaha <itsrohit132004@gmail.com>
2026-07-20 11:48:47 +0530

    data cleaning

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-df= pd.read_csv("Telecom.csv")
-print(df.head())
+df= pd.read_csv("telco_churn.csv")
+
+
+
+
+# print("TELECONNECT INDIA — CHURN ANALYSIS")
+
+
+
+# print(f"\nShape          : {df.shape}")
+# print(f"Columns        : {df.columns.tolist()}")
+# print(f"\nData Types:\n{df.dtypes}")
+# print(f"\nMissing Values:\n{df.isnull().sum()}")
+# print(f"\nFirst 5 rows:\n{df.head()}")
+
+
+# print("\n=== CHURN DISTRIBUTION ===")
+# print(df['Churn'].value_counts())
+# print(f"Churn Rate: {(df['Churn']=='Yes').mean()*100:.2f}%") 
+
+
+
+ 
